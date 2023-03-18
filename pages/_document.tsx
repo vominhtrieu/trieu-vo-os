2c5974e569558642b640ab80494c639057466566
vominhtrieu <minhtrieuvo600@gmail.com>
2023-03-18 21:56:39 +0700

    Update preview image

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <meta property="og:title" content="Trieu Vo OS" />
+        <meta property="og:description" content="A software engineer's portfolio presented as a simulated website, mimicking an operating system interface." />
+        <meta property="og:image" content="https://trieuvo.netlify.com/Wallpaper.jpg" />
         <link href='https://fonts.googleapis.com/css?family=Outfit&display=optional' rel='stylesheet' />
       </Head>
       <body>
