c79cddf4542dd8a4f56458cc3e02a18525c69cfb
vominhtrieu <minhtrieuvo600@gmail.com>
2023-03-18 22:20:41 +0700

    Minor fix

--- components/FileManager.tsx
+++ components/FileManager.tsx
@@ -5,7 +5,7 @@ import { useContext } from "react";
 import Window, { WindowProps } from "./Window";
 
 export default function FileManager(props: WindowProps) {
-  const { setFileManagerVisible, setSelectedPDF } = useContext(SystemContext);
+  const { setFileManagerVisible, setSelectedPDF, setSelectedWindow } = useContext(SystemContext);
   return (
     <Window {...props} onClose={() => {
       console.log(props);
@@ -13,15 +13,16 @@ export default function FileManager(props: WindowProps) {
     }}>
       <Box display="flex" flexWrap="wrap">
         <DesktopShortcut title="CV" icon="/file.svg" textColor="black" onClick={() => {
+          setSelectedWindow(document.getElementById("pdfViewerWindow"));
           setSelectedPDF("/cv.pdf")
         }} />
-        <DesktopShortcut title="Gmail" icon="/gmail.svg" textColor="black" onClick={()=>{
+        <DesktopShortcut title="Gmail" icon="/gmail.svg" textColor="black" onClick={() => {
           window.open("mailto:minhtrieuvo600@gmail.com");
         }} />
         <DesktopShortcut title="LinkedIn" icon="/linkedin.svg" textColor="black" onClick={() => {
           window.open("https://www.linkedin.com/in/vominhtrieu/");
         }} />
-        <DesktopShortcut title="Github" icon="/github.svg" textColor="black"  onClick={() => {
+        <DesktopShortcut title="Github" icon="/github.svg" textColor="black" onClick={() => {
           window.open("https://www.github.com/vominhtrieu");
         }} />
       </Box>
